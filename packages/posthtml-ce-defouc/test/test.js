a22e0240e24f8a9ead8bc5a2a25f8cda84600d5d
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-11-25 14:04:57 +0100

    test(plugin): add builtin tests

--- packages/posthtml-ce-defouc/test/test.js
+++ packages/posthtml-ce-defouc/test/test.js
@@ -7,6 +7,7 @@ const path = require('path')
 const posthtml = require('posthtml')
 const fixtures = path.join(__dirname, 'fixtures')
 
+// autonomous custom elements
 test('Autonomous Custom Element default inline style', (t) => {
   return compare(t, 'autonomous-default-inline-style')
 })
@@ -36,6 +37,40 @@ test('Autonomous Custom Element custom hash multi inline style', (t) => {
 
 test('Autonomous Custom Element custom class', (t) => {
   return compare(t, 'autonomous-custom-class', {
+    class: 'custom-elements-defouc'
+  })
+})
+
+// extended builtin elements
+test('Extended Builtin Element default inline style', (t) => {
+  return compare(t, 'builtin-default-inline-style')
+})
+
+test('Extended Builtin Element custom inline style', (t) => {
+  return compare(t, 'builtin-custom-inline-style', {
+    style: 'display: none'
+  })
+})
+
+test('Extended Builtin Element custom hash inline style', (t) => {
+  return compare(t, 'builtin-custom-hash-inline-style', {
+    style: {
+      display: 'none'
+    }
+  })
+})
+
+test('Extended Builtin Element custom hash multi inline style', (t) => {
+  return compare(t, 'builtin-custom-hash-multi-inline-style', {
+    style: {
+      display: 'none',
+      visibility: 'hidden'
+    }
+  })
+})
+
+test('Extended Builtin Element custom class', (t) => {
+  return compare(t, 'builtin-custom-class', {
     class: 'custom-elements-defouc'
   })
 })
