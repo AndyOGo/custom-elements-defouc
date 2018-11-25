203324f1654c48c9e70217a7a42fe6ccbc6bd12d
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-11-25 13:45:20 +0100

    test(plugin): add test for custom string inline style

--- packages/posthtml-ce-defouc/test/test.js
+++ packages/posthtml-ce-defouc/test/test.js
@@ -11,6 +11,12 @@ test('Autonomous Custom Element default inline style', (t) => {
   return compare(t, 'autonomous-default-inline-style')
 })
 
+test('Autonomous Custom Element custom inline style', (t) => {
+  return compare(t, 'autonomous-custom-inline-style', {
+    style: 'display: none'
+  })
+})
+
 function compare (t, name, options) {
   const html = readFileSync(path.join(fixtures, `${name}.html`), 'utf8')
   const expected = readFileSync(path.join(fixtures, `${name}.expected.html`), 'utf8')
