120a62e4df17a0fbe5d79c90d1b4759fe1700486
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-11-25 12:40:02 +0100

    test(plugin): add first default test

--- packages/posthtml-ce-defouc/test/test.js
+++ packages/posthtml-ce-defouc/test/test.js
@@ -7,15 +7,15 @@ const path = require('path')
 const posthtml = require('posthtml')
 const fixtures = path.join(__dirname, 'fixtures')
 
-test('basic', (t) => {
-  return compare(t, 'basic')
+test('Autonomous Custom Element default inline style', (t) => {
+  return compare(t, 'autonomous-default-inline-style')
 })
 
-function compare (t, name) {
+function compare (t, name, options) {
   const html = readFileSync(path.join(fixtures, `${name}.html`), 'utf8')
   const expected = readFileSync(path.join(fixtures, `${name}.expected.html`), 'utf8')
 
-  return posthtml([plugin()])
+  return posthtml([plugin(options)])
     .process(html)
     .then((res) => t.truthy(res.html === expected))
 }
