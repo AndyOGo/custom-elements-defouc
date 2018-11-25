095dc92a4cc3aacdce396965dde98a7414bdc365
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-11-25 16:18:43 +0100

    test(hoc): added class name tests

--- packages/with-custom-elements-defouc/tests/index-test.js
+++ packages/with-custom-elements-defouc/tests/index-test.js
@@ -4,24 +4,55 @@ import withCustomElementsDeFouc from 'src/index'
 
 class CustomElementFixture {}
 
-function createFakeCustomElement() {
-  const div = document.createElement('div')
-
-  div.style.visibility = 'hidden'
-
-  return div
-}
-
 describe('withCustomElementsDeFouc', () => {
   it('removes default visibility inline style upon connection', () => {
     const CustomElementFixtureHoced = withCustomElementsDeFouc(CustomElementFixture)
     const ce = new CustomElementFixtureHoced()
-    const fakeCeContext = createFakeCustomElement()
+    const div = document.createElement('div')
+    div.style.visibility = 'hidden'
 
-    expect(fakeCeContext.style.visibility).toEqual('hidden')
+    expect(div.style.visibility).toEqual('hidden')
 
-    ce.connectedCallback.call(fakeCeContext)
+    ce.connectedCallback.call(div)
 
-    expect(fakeCeContext.style.visibility).toBeFalsy()
+    expect(div.style.visibility).toBeFalsy()
+  })
+
+  it('removes custom inline style upon connection', () => {
+    const CustomElementFixtureHoced = withCustomElementsDeFouc(CustomElementFixture, {
+      style: 'display'
+    })
+    const ce = new CustomElementFixtureHoced()
+    const div = document.createElement('div')
+    div.style.display = 'none'
+
+    expect(div.style.display).toEqual('none')
+
+    ce.connectedCallback.call(div)
+
+    expect(div.style.display).toBeFalsy()
+  })
+
+  it('removes class name upon connection', () => {
+    const CustomElementFixtureHoced = withCustomElementsDeFouc(CustomElementFixture, {
+      className: 'foo'
+    })
+    const ce = new CustomElementFixtureHoced()
+    const div = document.createElement('div')
+
+    div.className = 'foo'
+    expect(div.className).toEqual('foo')
+    ce.connectedCallback.call(div)
+    expect(div.className).toEqual(' ')
+
+    div.className = 'bar foo baz'
+    expect(div.className).toEqual('bar foo baz')
+    ce.connectedCallback.call(div)
+    expect(div.className).toEqual('bar baz')
+
+    div.className = 'bar foo'
+    expect(div.className).toEqual('bar foo')
+    ce.connectedCallback.call(div)
+    expect(div.className).toEqual('bar ')
   })
 })
